22bf840faf75531e85ef84e6ca5904e915a1ae2d
Arınç Demir <68223368+arincdemir@users.noreply.github.com>
2024-08-13 18:14:21 +0300

    Wrote interface tests

---
+++ Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis.Test/NameChecker/InterfaceNameUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = TaleworldsCodeAnalysis.Test.CSharpCodeFixVerifier<
+    TaleworldsCodeAnalysis.NameChecker.InterfaceNameChecker,
+    TaleworldsCodeAnalysis.TaleworldsCodeAnalysisCodeFixProvider>;
+
+namespace TaleworldsCodeAnalysis.Test.NameChecker
+{
+    [TestClass]
+    public class InterfaceNameUnitTests
+    {
+        [TestMethod]
+        public async Task InterfaceNoWarningTest()
+        {
+            var test = @"
+            public interface IBase
+            {
+            }
+            public interface ITest : IBase
+            {
+            }";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task InterfaceWarningTest()
+        {
+            var test = @"
+            public interface {|#0:iTest|} : IBase {};
+            internal interface {|#1:Test|} : IBase {}
+            public interface {|#2:_test|} : iBase;
+            ";
+
+            var expected1 = VerifyCS.Diagnostic("InterfaceNameChecker").WithLocation(0).WithArguments("iTest");
+            var expected2 = VerifyCS.Diagnostic("InterfaceNameChecker").WithLocation(1).WithArguments("Test");
+            var expected3 = VerifyCS.Diagnostic("InterfaceNameChecker").WithLocation(2).WithArguments("_test");
+
+            await VerifyCS.VerifyAnalyzerAsync(test, expected1, expected2, expected3);
+        }
+
+    }
+}
