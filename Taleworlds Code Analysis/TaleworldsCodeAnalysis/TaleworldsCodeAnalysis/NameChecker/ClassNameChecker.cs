e32a4dbd4dc618c673b755fb27b7337f8e002b83
Arınç Demir <68223368+arincdemir@users.noreply.github.com>
2024-08-13 15:52:41 +0300

    Class protected case is fixed

--- Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis/NameChecker/ClassNameChecker.cs
+++ Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis/NameChecker/ClassNameChecker.cs
@@ -1,30 +1,26 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Text;
 
 namespace TaleworldsCodeAnalysis.NameChecker
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class ClassNameChecker : DiagnosticAnalyzer
     {
-        public const string DiagnosticId = "ClassNameChecker";
+        public const string NameDiagnosticId = "ClassNameChecker";
+        public const string ModifierDiagnosticId = "ClassModifierChecker";
+
         private static readonly LocalizableString _title = new LocalizableResourceString(nameof(NameCheckerResources.ClassNameCheckerTitle), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
         private static readonly LocalizableString _messageFormat = new LocalizableResourceString(nameof(NameCheckerResources.ClassNameCheckerMessageFormat), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
         private static readonly LocalizableString _description = new LocalizableResourceString(nameof(NameCheckerResources.ClassNameCheckerDescription), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
+        private static readonly LocalizableString _modifierMessageFormat = new LocalizableResourceString(nameof(NameCheckerResources.ClassModifierCheckerMessageFormat), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
 
         private const string _category = "Naming";
 
-        private static readonly DiagnosticDescriptor _nameRule = new DiagnosticDescriptor(DiagnosticId, _title, _messageFormat, _category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: _description);
-         // Fix for protected classes not being allowed
-        public const string ModifierDiagnosticId = "ClassModifierChecker";
-        private static readonly LocalizableString _modifierMessageFormat = new LocalizableResourceString(nameof(NameCheckerResources.ClassModifierCheckerMessageFormat), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
+        private static readonly DiagnosticDescriptor _nameRule = new DiagnosticDescriptor(NameDiagnosticId, _title, _messageFormat, _category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: _description);
         private static readonly DiagnosticDescriptor _modifierRule = new DiagnosticDescriptor(ModifierDiagnosticId, _title, _modifierMessageFormat, _category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: _description);
 
-
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_nameRule);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_nameRule, _modifierRule);
 
         public override void Initialize(AnalysisContext context)
         {
@@ -40,7 +36,7 @@ namespace TaleworldsCodeAnalysis.NameChecker
             // TODO : Implement protected classes to not be allowed
 
             if (classSymbol.DeclaredAccessibility == Accessibility.Private ||
-                 classSymbol.DeclaredAccessibility == Accessibility.Internal)
+                classSymbol.DeclaredAccessibility == Accessibility.Internal)
             {
                 if (!NameCheckerLibrary.IsUnderScoreCase(classSymbol.Name))
                 {
