5e29ff84296db6acbb29a582d24baa50a8194790
Arınç Demir <68223368+arincdemir@users.noreply.github.com>
2024-08-28 17:02:32 +0300

    Made it so the build of the command comes to the right place.

--- Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis.CodeFixes/TaleworldsCodeAnalysisCodeFixProvider.cs
+++ Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis.CodeFixes/TaleworldsCodeAnalysisCodeFixProvider.cs
@@ -28,6 +28,7 @@ namespace TaleworldsCodeAnalysis
             }
         }
 
+
         public sealed override FixAllProvider GetFixAllProvider()
         {
             // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
