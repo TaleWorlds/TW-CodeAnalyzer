8636bfa148900a206f134e0904a5669ef16d730e
Arınç Demir <68223368+arincdemir@users.noreply.github.com>
2024-09-02 12:46:27 +0300

    Seperated class 'protected' rule to its own file

--- Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis/NameChecker/ClassNameChecker.cs
+++ Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis/NameChecker/ClassNameChecker.cs
@@ -12,22 +12,18 @@ namespace TaleworldsCodeAnalysis.NameChecker
     public class ClassNameChecker : DiagnosticAnalyzer
     {
         public static string NameDiagnosticId =>_nameDiagnosticId;
-        public static string ModifierDiagnosticId=>_modifierDiagnosticId;
 
         private const string _nameDiagnosticId = "TW2000";
-        private const string _modifierDiagnosticId = "TW2001";
 
         private static readonly LocalizableString _title = new LocalizableResourceString(nameof(NameCheckerResources.ClassNameCheckerTitle), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
         private static readonly LocalizableString _messageFormat = new LocalizableResourceString(nameof(NameCheckerResources.ClassNameCheckerMessageFormat), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
         private static readonly LocalizableString _description = new LocalizableResourceString(nameof(NameCheckerResources.ClassNameCheckerDescription), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
-        private static readonly LocalizableString _modifierMessageFormat = new LocalizableResourceString(nameof(NameCheckerResources.ClassModifierCheckerMessageFormat), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
 
         private const string _category = "Naming";
 
         private static readonly DiagnosticDescriptor _nameRule = new DiagnosticDescriptor(_nameDiagnosticId, _title, _messageFormat, _category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: _description);
-        private static readonly DiagnosticDescriptor _modifierRule = new DiagnosticDescriptor(_modifierDiagnosticId, _title, _modifierMessageFormat, _category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: _description);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_nameRule, _modifierRule);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_nameRule);
 
         public override void Initialize(AnalysisContext context)
         {
@@ -39,8 +35,6 @@ namespace TaleworldsCodeAnalysis.NameChecker
 
         private void _analyzer(SyntaxNodeAnalysisContext context)
         {
-
-            
 
             var nameNode = (ClassDeclarationSyntax) context.Node;
             var nameString = nameNode.Identifier.Text;
@@ -73,10 +67,6 @@ namespace TaleworldsCodeAnalysis.NameChecker
                     context.ReportDiagnostic(Diagnostic.Create(_nameRule, location, properties.ToImmutableDictionary(), nameString,
                         PascalCaseBehaviour.Instance.FixThis(nameString)));
                 }
-            }
-            else
-            {
-                context.ReportDiagnostic(Diagnostic.Create(_modifierRule, location, nameString));
             }
         }
     }
---
+++ Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis/OtherCheckers/ClassAccessibilityChecker.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using TaleworldsCodeAnalysis.NameChecker.Conventions;
+
+namespace TaleworldsCodeAnalysis.NameChecker
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ClassAccessibilityChecker : DiagnosticAnalyzer
+    {
+        public static string ModifierDiagnosticId => _modifierDiagnosticId;
+
+        private const string _modifierDiagnosticId = "TW2001";
+
+        private static readonly LocalizableString _title = new LocalizableResourceString(nameof(NameCheckerResources.ClassModifierCheckerTitle), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
+        private static readonly LocalizableString _description = new LocalizableResourceString(nameof(NameCheckerResources.ClassModifierCheckerDescription), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
+        private static readonly LocalizableString _modifierMessageFormat = new LocalizableResourceString(nameof(NameCheckerResources.ClassModifierCheckerMessageFormat), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
+
+        private const string _category = "Accessibility";
+
+        private static readonly DiagnosticDescriptor _modifierRule = new DiagnosticDescriptor(_modifierDiagnosticId, _title, _modifierMessageFormat, _category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: _description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_modifierRule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(_analyzer, SyntaxKind.ClassDeclaration);
+
+        }
+
+        private void _analyzer(SyntaxNodeAnalysisContext context)
+        {
+            var nameNode = (ClassDeclarationSyntax)context.Node;
+            var nameString = nameNode.Identifier.Text;
+            var accessibility = nameNode.Modifiers.First();
+            var location = nameNode.Identifier.GetLocation();
+
+            if (PreAnalyzerConditions.Instance.IsNotAllowedToAnalyze(context)) return;
+
+
+            var properties = new Dictionary<string, string>
+            {
+                { "Name", nameString},
+            };
+
+            if (accessibility.IsKind(SyntaxKind.ProtectedKeyword))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(_modifierRule, location, properties.ToImmutableDictionary(), nameString));
+            }
+        }
+    }
+}
