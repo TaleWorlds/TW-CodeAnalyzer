a84e9c271400c286fab5c01e6c1472b391de1017
Arınç Demir <68223368+arincdemir@users.noreply.github.com>
2024-09-09 17:22:21 +0300

    A warning is shown if a list field is returned.

--- Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis/OtherCheckers/MutableListReturnChecker.cs
+++ Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis/OtherCheckers/MutableListReturnChecker.cs
@@ -3,54 +3,66 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System.Collections.Immutable;
+using System.Data;
+using System.Linq;
 using TaleworldsCodeAnalysis.NameChecker;
 
 namespace TaleworldsCodeAnalysis.OtherCheckers
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class MutebleAccessibilityChecker : DiagnosticAnalyzer
+    public class MutebleFieldReturnChecker : DiagnosticAnalyzer
     {
         public static string DiagnosticId => _diagnosticId;
-        private const string _diagnosticId = "TW2201";
-        private static readonly LocalizableString _title = new LocalizableResourceString(nameof(NameCheckerResources.FieldAccessibilityCheckerTitle), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
-        private static readonly LocalizableString _messageFormat = new LocalizableResourceString(nameof(NameCheckerResources.FieldAccessibilityCheckerMessageFormat), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
-        private static readonly LocalizableString _description = new LocalizableResourceString(nameof(NameCheckerResources.FieldAccessibilityCheckerDescription), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
-        private const string _accessibilityCategory = "Encapsulation";
+        private const string _diagnosticId = "TW2204";
+        private static readonly string _title = "MutableFieldReturnChecker Title";
+        private static readonly string _messageFormat = "You should not return a 'List' that is a field. It can be modified outside.";
+        private static readonly string _description = "Description";
+        private const string _category = "Encapsulation";
 
-        private static readonly DiagnosticDescriptor _accessibilityRule = new DiagnosticDescriptor(_diagnosticId, _title, _messageFormat, _accessibilityCategory, DiagnosticSeverity.Error, isEnabledByDefault: true, description: _description);
+        private static readonly DiagnosticDescriptor _rule = new DiagnosticDescriptor(_diagnosticId, _title, _messageFormat, _category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: _description);
 
 
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
         {
-            get { return ImmutableArray.Create(_accessibilityRule); }
+            get { return ImmutableArray.Create(_rule); }
         }
 
         public override void Initialize(AnalysisContext context)
         {
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
-            context.RegisterSyntaxNodeAction(_analyzer, SyntaxKind.MethodDeclaration);
+            context.RegisterSyntaxNodeAction(_returnAnalyzer, SyntaxKind.ReturnStatement);
         }
 
-        private void _analyzer(SyntaxNodeAnalysisContext context)
+        private void _returnAnalyzer(SyntaxNodeAnalysisContext context)
         {
-
             if (PreAnalyzerConditions.Instance.IsNotAllowedToAnalyze(context, DiagnosticId)) return;
+            var node = (ReturnStatementSyntax)context.Node;
 
-            var node = (MethodDeclarationSyntax)context.Node;
-            var returnType = node.ReturnType;
+
+            var includingMethod = (MethodDeclarationSyntax)node.AncestorsAndSelf().FirstOrDefault(elem => elem.IsKind(SyntaxKind.MethodDeclaration));
+            var returnType = includingMethod.ReturnType;
             if (!(returnType is GenericNameSyntax)) return;
-            var identifier = ((GenericNameSyntax)returnType).Identifier;
+            var returnTypeIdentifier = ((GenericNameSyntax)returnType).Identifier;
+            if (returnTypeIdentifier.ValueText != "List") return;
 
-            if (identifier.ValueText != "List") return;
+            var returnExpression = node.Expression;
+            if (!(returnExpression is IdentifierNameSyntax)) return;
+            var returnedIdentifierName = ((IdentifierNameSyntax)returnExpression).Identifier.ValueText;
 
-            var statements = node.Body.Statements;
-
-            var a = 1;
-           
-
+            var includingClass = (ClassDeclarationSyntax)node.AncestorsAndSelf().FirstOrDefault(elem => elem.IsKind(SyntaxKind.ClassDeclaration));
+            var classFields = includingClass.Members.Where(elem => elem.IsKind(SyntaxKind.FieldDeclaration)).Select(elem => (FieldDeclarationSyntax)elem);
+            var returnedValueIsField = classFields.Any(elem => elem.Declaration.Variables.Any(variable => variable.Identifier.ValueText == returnedIdentifierName));
             
+            
+            if (returnedValueIsField)
+            {
+               
+                var diagnosticLocation = node.GetLocation();
+                context.ReportDiagnostic(Diagnostic.Create(_rule, diagnosticLocation));
+            }
+
         }
 
     }
