09d7d671097a6d0e09000b9023e7ac00c8a4c82b
Arınç Demir <68223368+arincdemir@users.noreply.github.com>
2024-09-09 14:28:18 +0300

    Created the file for checking if a method returns a list

---
+++ Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis/OtherCheckers/MutableListReturnChecker.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using TaleworldsCodeAnalysis.NameChecker;
+
+namespace TaleworldsCodeAnalysis.OtherCheckers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MutebleAccessibilityChecker : DiagnosticAnalyzer
+    {
+        public static string DiagnosticId => _diagnosticId;
+        private const string _diagnosticId = "TW2202";
+        private static readonly LocalizableString _title = new LocalizableResourceString(nameof(NameCheckerResources.FieldAccessibilityCheckerTitle), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
+        private static readonly LocalizableString _messageFormat = new LocalizableResourceString(nameof(NameCheckerResources.FieldAccessibilityCheckerMessageFormat), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
+        private static readonly LocalizableString _description = new LocalizableResourceString(nameof(NameCheckerResources.FieldAccessibilityCheckerDescription), NameCheckerResources.ResourceManager, typeof(NameCheckerResources));
+        private const string _accessibilityCategory = "Encapsulation";
+
+        private static readonly DiagnosticDescriptor _accessibilityRule = new DiagnosticDescriptor(_diagnosticId, _title, _messageFormat, _accessibilityCategory, DiagnosticSeverity.Error, isEnabledByDefault: true, description: _description);
+
+
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get { return ImmutableArray.Create(_accessibilityRule); }
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(_analyzer, SyntaxKind.MethodDeclaration);
+        }
+
+        private void _analyzer(SyntaxNodeAnalysisContext context)
+        {
+
+            if (PreAnalyzerConditions.Instance.IsNotAllowedToAnalyze(context, DiagnosticId)) return;
+
+            var node = (MethodDeclarationSyntax)context.Node;
+            var returnType = node.ReturnType;
+
+            var a = 1;
+           
+
+            
+        }
+
+    }
+}
