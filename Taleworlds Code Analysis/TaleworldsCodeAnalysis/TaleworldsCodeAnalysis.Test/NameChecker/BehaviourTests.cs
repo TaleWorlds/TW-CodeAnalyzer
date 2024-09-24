c0571625dc0dd18f2cb41f265cb2be68840b8f97
Berkay1119 <78219728+Berkay1119@users.noreply.github.com>
2024-09-24 11:19:23 +0300

    new unit tests for regex

--- Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis.Test/NameChecker/BehaviourTests.cs
+++ Taleworlds Code Analysis/TaleworldsCodeAnalysis/TaleworldsCodeAnalysis.Test/NameChecker/BehaviourTests.cs
@@ -30,11 +30,42 @@ namespace TaleworldsCodeAnalysis.Test.NameChecker
         {
             Assert.IsTrue(!CamelCaseBehaviour.Instance.IsMatching("TaleWorlds"));
             Assert.IsTrue(CamelCaseBehaviour.Instance.IsMatching("codeFixTW"));
+            Assert.IsTrue(CamelCaseBehaviour.Instance.IsMatching("user1Name"));
+            Assert.IsTrue(!CamelCaseBehaviour.Instance.IsMatching("hello_world"));
+            Assert.IsTrue(!CamelCaseBehaviour.Instance.IsMatching("user@name"));
+            Assert.IsTrue(CamelCaseBehaviour.Instance.IsMatching("myVarName123"));
         }
 
         [TestMethod]
         public void CamelWhiteList()
         {
+        }
+
+        [TestMethod]
+        public void PascalIsMatching()
+        {
+            Assert.IsTrue(PascalCaseBehaviour.Instance.IsMatching("HelloWorld"));
+            Assert.IsTrue(PascalCaseBehaviour.Instance.IsMatching("User1Name"));
+            Assert.IsTrue(!PascalCaseBehaviour.Instance.IsMatching("helloworld"));
+            Assert.IsTrue(!PascalCaseBehaviour.Instance.IsMatching("HELLOWORLD"));
+            Assert.IsTrue(!PascalCaseBehaviour.Instance.IsMatching("Hello_World"));
+            Assert.IsTrue(!PascalCaseBehaviour.Instance.IsMatching("helloWorld"));
+            Assert.IsTrue(PascalCaseBehaviour.Instance.IsMatching("MyVariableName"));
+            Assert.IsTrue(!PascalCaseBehaviour.Instance.IsMatching("User@Name"));
+            Assert.IsTrue(PascalCaseBehaviour.Instance.IsMatching("MyVarName123"));
+        }
+        
+        [TestMethod]
+        public void UnderScorIsMatching()
+        {
+            Assert.IsTrue(UnderScoreCaseBehaviour.Instance.IsMatching("_helloWorld"));
+            Assert.IsTrue(UnderScoreCaseBehaviour.Instance.IsMatching("_user1Name"));
+            Assert.IsTrue(UnderScoreCaseBehaviour.Instance.IsMatching("_helloworld"));
+            Assert.IsTrue(!UnderScoreCaseBehaviour.Instance.IsMatching("_HELLOWORLD"));
+            Assert.IsTrue(!UnderScoreCaseBehaviour.Instance.IsMatching("_hello@world"));
+            Assert.IsTrue(!UnderScoreCaseBehaviour.Instance.IsMatching("_helloWorld_"));
+            Assert.IsTrue(!UnderScoreCaseBehaviour.Instance.IsMatching("__helloWorld"));
+            Assert.IsTrue(!UnderScoreCaseBehaviour.Instance.IsMatching("_My_Var_Name"));
         }
 
     }
